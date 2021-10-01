4ab5564e1618d286e5dc385015798196c1ff9963
Tingerhedgehog <Tingerhedgehog351@gmail.com>
2021-10-02 00:53:35 +0200

    Random nonsense

---
+++ MaxwellWasHere.js
@@ -0,0 +1,12 @@
+//#region madness
+
+var wow = "🥵";
+
+//#endregion
+
+//#region educational nonsense
+for (let CommunismIsCrazy = 0; CommunismIsCrazy < 10; CommunismIsCrazy++)
+{
+    console.log(wow);   
+}
+//#endregion
